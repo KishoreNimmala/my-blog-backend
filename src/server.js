f4ea235678a0f6e4b411d935cc833bf464cf92ef
Kishore Nimmala <nkishore779@gmail.com>
2020-08-06 17:20:17 +0530

    Adding MongoDB to Express

--- src/server.js
+++ src/server.js
@@ -1,29 +1,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import {MongoClient} from 'mongodb';
 const app=express();
 
-const articlesInfo={
-    'learn-react':{
-        upvotes:0,
-        comments:[],
-    },
-    'learn-node':{
-        upvotes:0,
-        comments:[],
-    },
-    'my-thoughts-on-resumes':{
-        upvotes:0,
-        comments:[],
-    }
-}
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
+app.get('/api/articles/:name', async (req,res)=>{
+    try {
+        const articleName=req.params.name;
+        const client= await  MongoClient.connect('mongodb://localhost:27017',{useNewUrlParser:true});
+        const db=client.db('my-blog');
+
+        const articleInfo=await db.collection('articles').findOne({name:articleName});
+        res.status(200).json(articleInfo);
+        client.close();
+    } catch (error) {
+        res.status(500).json({message:'Error connecting to db', error})
+    }
+
+    
+});
 
 app.post('/api/articles/:name/upvote',(req,res)=>{
     const articleName=req.params.name;
     articlesInfo[articleName].upvotes+=1;
     res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`);
-})
+});
 
 app.post('/api/articles/:name/add-comment',(req,res)=>{
     const {username,text}=req.body;
